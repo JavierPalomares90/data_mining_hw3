faa12ce11173b17dcfc32703d8855d3e8a195afe
Javier Palomares <javier_palomares@homedepot.com>
2019-03-28 10:57:03 -0500

    added method to synthesize paragraph. what is cid107. seeing that alot in words

--- entropy.py
+++ entropy.py
@@ -2,8 +2,10 @@
 # Must be executed after scraper.py writes word_freqs.txt
 
 WORD_FREQ_FILE= 'word_freqs.txt'
+PARAGRAPH_LEN = 20
 K = 1.0
 import math
+import numpy as np
 
 
 def get_word_prob():
@@ -30,10 +32,29 @@ def compute_entropy(word_probs):
         H += h
     return H
 
+def verify_word_probs(word_probs):
+    # verify the word probs sum up to 1.0
+    s = 0.0
+    for word,p in word_probs.items():
+        s += p
+    print("total prob: {}".format(s))
+
+def first_order_synthesized_paragraph(word_probs,par_len):
+    # synthesize a paragraph of the given length with the
+    # given word probabilities
+    words = list(word_probs.keys())
+    weights = list(word_probs.values())
+    paragraph = np.random.choice(words,par_len,p=weights)
+    return ' '.join(paragraph)
+
+
+
 def main():
     word_probs = get_word_prob()
+    verify_word_probs(word_probs)
     H = compute_entropy(word_probs)
     print("The computed entropy for the pdf text is {}".format(H))
+    print(first_order_synthesized_paragraph(word_probs,20))
 
 if __name__=='__main__':
     main()
